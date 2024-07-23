e74b1c65c69e538b2a3b5ed0bb11a605907c94bf
Iskustvo <iskustvo@yahoo.com>
2024-07-23 03:59:23 +0200

    fix(diagnostics): update configuration and stop using deprecated functions

--- lua/user/diagnostics.lua
+++ lua/user/diagnostics.lua
@@ -1,30 +1,21 @@
-local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
 vim.diagnostic.config({
+    underline = true,
     virtual_text = true,
     signs = {
-        active = signs,
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.INFO] = "",
+        }
+    },
+    float = {
+        header = "",
+        source = true,
+        border = "rounded",
     },
     update_in_insert = false,
-    underline = true,
     severity_sort = true,
-    float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
-    },
 })
 
 require("trouble").setup()
